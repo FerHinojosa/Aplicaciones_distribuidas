8534eee933c58507ef5d588c094ca46042e2a1cb
FerHinojosa <fernando.hinojosa@live.com>
2024-11-15 18:18:17 -0400

    Add files via upload

---
+++ Sprint II/load_hdfs.sh
@@ -0,0 +1,4 @@
+hdfs dfs -mkdir /data/airlines
+hdfs dfs -put ../2024_1.csv /data/airlines/2024_1.csv 
+hdfs dfs -put ../2024_2.csv /data/airlines/2024_2.csv
+hdfs dfs -put ../2024_3.csv /data/airlines/2024_3.csv
